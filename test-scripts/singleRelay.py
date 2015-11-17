7768e5d758213c1e1ce5eee45a3eeecc2b83d775
Henry Saniuk <henry@henrysaniuk.com>
2015-11-17 15:37:28 -0500

    Updated test script to make it easier to change pin test

--- test-scripts/singleRelay.py
+++ test-scripts/singleRelay.py
@@ -1,14 +1,16 @@
 import RPi.GPIO as GPIO, time
 
+pin = 12
+
 GPIO.setmode(GPIO.BOARD)
 print("Getting Started")
 time.sleep(3.0)
 
 print("Turning on 1")
-GPIO.setup(11, GPIO.OUT)
-GPIO.output(11, True)
+GPIO.setup(pin, GPIO.OUT)
+GPIO.output(pin, True)
 time.sleep(2.0)
 print("Turning off 1")
-GPIO.output(11, False)
+GPIO.output(pin, False)
 
 GPIO.cleanup()
