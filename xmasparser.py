e3181c8e3aa6d3df32b321435ed473f122720456
Henry Saniuk <henry@henrysaniuk.com>
2015-11-19 21:01:50 -0500

    changed audio again

--- xmasparser.py
+++ xmasparser.py
@@ -6,7 +6,7 @@ try:
     mp3 = str(sys.argv[2])
     print 'config file =', config
     print 'mp3 file =', mp3
-    systemCall = 'omxplayer -o local ', mp3
+    systemCall = 'omxplayer -o local ' + str(mp3)
     os.system(systemCall)
 except IndexError:
     print 'Usage: python xmasparser.py <config file> <mp3 file>'
