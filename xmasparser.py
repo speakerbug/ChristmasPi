835d65dd644d3f6e2228b8c7e7387342bfd7c4b4
Henry Saniuk <henry@henrysaniuk.com>
2015-11-28 18:25:57 -0500

    Added more comments

--- xmasparser.py
+++ xmasparser.py
@@ -11,6 +11,8 @@ import pygame
 # Defines the mapping of the GPIO pins on the Pi in order of channel
 pin_map = [11,12,13,15,16,18,22,7,29,31,32,33,35,36,37,38]
 
+# Defines if program should be in debug mode and 
+# print out channels it is turning on and off
 debug = True
 
 try:
@@ -28,6 +30,7 @@ for i in range(0,16):
     GPIO.setup(pin_map[i], GPIO.OUT)
     GPIO.output(pin_map[i],False)           
 
+# Load and play MP3 file
 pygame.mixer.init()
 pygame.mixer.music.load(mp3)
 pygame.mixer.music.play()
@@ -51,13 +54,13 @@ while True :
         # change the pin state
         if next_step[2] == "1":
             if debug:
-                print("Turning on " + next_step[1])
+                print("Turning on " + str(next_step[1]))
             GPIO.output(pin_map[int(next_step[1])-1],True)
         else:
             if debug:
-                print("Turning off " + next_step[1])
+                print("Turning off " + str(next_step[1]))
             GPIO.output(pin_map[int(next_step[1])-1],False)
-        # Increment what step we are on
+        # Increment what step we are on in the sequence
         step += 1
 
     # This is used to check for END
