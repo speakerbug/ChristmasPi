8a1eaf1f8ad2fdf28baca6dad1a37c21240f8cdb
Henry Saniuk <henry@henrysaniuk.com>
2015-11-28 17:54:32 -0500

    Added debug mode to xmasparser

--- xmasparser.py
+++ xmasparser.py
@@ -11,17 +11,20 @@ import pygame
 # Defines the mapping of the GPIO pins on the Pi in order of channel
 pin_map = [11,12,13,15,16,18,22,7,29,31,32,33,35,36,37,38]
 
+debug = True
+
 try:
     config = str(sys.argv[1])
     mp3 = str(sys.argv[2])
-    print 'config file =', config
-    print 'mp3 file =', mp3
+    if debug:
+        print 'config file =', config
+        print 'mp3 file =', mp3
 except IndexError:
-    print 'Usage: python xmasparser.py <config file> <mp3 file>'
+    print 'Usage: python debugparser.py <config file> <mp3 file>'
     
 # Setup the board
 GPIO.setmode(GPIO.BOARD)
-for i in range(1,16):
+for i in range(0,15):
     GPIO.setup(pin_map[i], GPIO.OUT)
 time.sleep(1.0)
 
@@ -43,26 +46,23 @@ while True :
     next_step[1] = next_step[1].rstrip()
     cur_time = int(round(time.time()*1000)) - start_time
 
-    print(cur_time)
-    print(next_step[0])
     # time to run the command
     if int(next_step[0]) <= cur_time:
-        print next_step
-        # if the command is Relay 1-16 
-        if next_step[1] >= "1" and next_step[1] <= "16":
-            step += 1
-            # change the pin state
-            if next_step[2] == "1":
-                GPIO.output(pin_map[int(next_step[1])],True)
-            else:
-                GPIO.output(pin_map[int(next_step[1])],False)
+        # change the pin state
+        if next_step[2] == "1":
+            if debug:
+                print("Turning on " + next_step[1])
+            GPIO.output(pin_map[int(next_step[1])],True)
+        else:
+            if debug:
+                print("Turning off " + next_step[1])
+            GPIO.output(pin_map[int(next_step[1])],False)
+        # Increment what step we are on
+        step += 1
 
     # This is used to check for END
     if next_step[1].rstrip() == "END":
-        for i in range(1,12):
-            GPIO.output(pin_map[i],False)
         # Reached end - End loop
         break
-    # Increment what step we are on
 
 GPIO.cleanup()
